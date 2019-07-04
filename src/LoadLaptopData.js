36f1d98eca811beeca4c21e71946154b6cbb937e
Jack Broomer <jbroomer@umass.edu>
2019-07-04 07:46:40 -0400

    Improve selection functionality

--- src/LoadLaptopData.js
+++ src/LoadLaptopData.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputBase from '@material-ui/core/InputBase';
+
 import LaptopCard from './LaptopCard';
 
 const laptopData = [
@@ -47,22 +52,57 @@ const laptopData = [
 }
 ];
 
+const BootstrapInput = withStyles(theme => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+      marginLeft: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
   },
 }));
 
 export default function CenteredGrid() {
   const classes = useStyles();
   const [cards, setCards] = useState([]);
+  const [filter, setFilter] = useState('none');
 
-  const setCardState = () => {
+  const setInitialCardState = () => {
     setCards(laptopData.map((x) => {
       return <LaptopCard key = {x.id} item = {x} />
     }));
@@ -70,17 +110,38 @@ export default function CenteredGrid() {
   }
 
   useEffect(() => {
-    setCardState();
+    setInitialCardState();
   }, cards.length);
 
-  const showLaptopHeader = () => {
-    const header = cards.length > 0 ? (<h1>Laptops</h1>) : null;
-    return header;
+  const handleChange = (e) => {
+    setFilter(e.target.value);
+    if(e.target.value === 'All') {
+      setInitialCardState();
+    }
+    else {
+      setCards(laptopData.map((x) => {
+        return <LaptopCard key = {x.id} item = {x} />
+      }).filter((item) => {
+        return item.props.item.make === e.target.value;
+      }));
+    }
   }
 
   return (
     <div className={classes.root}>
-      {showLaptopHeader()}
+      <FormControl className={classes.margin}>
+        <InputLabel htmlFor="select-item">Laptop</InputLabel>
+        <NativeSelect
+          value={filter}
+          onChange={handleChange}
+          input={<BootstrapInput name="item" id="select-item" />}
+        >
+          <option value={'All'}>All</option >
+          <option value={'Apple'}>Apple</option>
+          <option value={'Dell'}>Dell</option>
+          <option value={'Chromebook'}>Chromebook</option>
+        </NativeSelect>
+      </FormControl>
       <Grid container spacing={3}>
         {cards}
       </Grid>
